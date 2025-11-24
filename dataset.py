91e1ea476d332686b16128ffc4927b18e067a747
hectorastrom <hector.astrom@gmail.com>
2025-11-24 20:16:25 +0000

    + helpful COD functions

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ tensor_transform = transforms.Compose([
     transforms.Resize((512, 512)),
     transforms.ToTensor(),
 ])
-LABEL_MAPPER = None
 
 def pytorch_transform_fn(examples):
     # examples is a dict of lists: {'image': [PIL, ...], 'label': [0, ...]}
@@ -14,30 +13,25 @@ def pytorch_transform_fn(examples):
     del examples['image'] # ensures we only have 2 cols for loader
     return examples
 
-def create_loader(batch_size=4, num_workers=1, split_name = 'train'):
-    global LABEL_MAPPER
+def build_COD_torch_dataset(split_name = 'train'):
     dataset = load_cod10k_lazy()[split_name]
-    LABEL_MAPPER = dataset.features['label']
     dataset.set_transform(pytorch_transform_fn)
-    loader = DataLoader(
-        dataset, 
-        batch_size = batch_size, 
-        shuffle = (split_name == 'train'),
-        num_workers = num_workers 
-    )
-    return loader
-
-def label_int2str(label_int : int):
-    return LABEL_MAPPER.int2str(label_int)
     
-
+    label_feature = dataset.features['label']
+    # Adding some metadata that's helpful
+    dataset.all_classes = label_feature.names 
+    dataset.label2str = label_feature.int2str
+    
+    return dataset
+    
 if __name__ == "__main__":
-    loader = create_loader(4, 1, 'train')
+    dataset = build_COD_torch_dataset('train')
+    loader = DataLoader(dataset, batch_size=4, num_workers=1, shuffle=True)
     
     for item in loader: # dict{'label', 'pixel_values'}
         img_tensor = item['pixel_values']
         label_int = item['label']
         print("Image tensor:", img_tensor.shape)
         print("Labels:", label_int)
-        print("Str labels:", label_int2str(label_int))
+        print("Str labels:", dataset.label2str(label_int))
         break
